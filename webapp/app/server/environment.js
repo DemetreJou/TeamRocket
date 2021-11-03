0b6d0aea4ae571924bd064e44d3cb2611436d2a0
Demetre <demetre.jouras@gmail.com>
2021-11-02 23:25:02 -0400

    remove unneeded config params

--- webapp/app/server/environment.js
+++ webapp/app/server/environment.js
@@ -1,10 +1,5 @@
 require('dotenv').config()
 module.exports = {
-  db: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  },
   webapp: {
     port: process.env.PORT,
     send_to_url: process.env.SEND_TO_URL,
