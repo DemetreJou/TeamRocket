d52eda4ba49fde4b7829d0edad54458b3e05bda6
Demetre <demetre.jouras@gmail.com>
2021-11-26 23:11:29 -0500

    create post endpoint

--- logging_server/app.js
+++ logging_server/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
 const PrismaClient = require('@prisma/client').PrismaClient;
 const prisma = new PrismaClient()
 
+const app = express();
+app.use(bodyParser.json())
+
 app.get("/", (req, res) => {
-  res.json({ message: "Basic distributed logging server" });
+  res.json({message: "Basic distributed logging server"});
 });
 
-app.post("/log", (req, res) => {
-  console.log(req.body);
+async function saveLog(log) {
+  return prisma.log.create({
+    data: {
+      message: log.message,
+      logLevel: log.level,
+      timestamp: log.timestamp,
+      machineId: log.machine_id,
+    }
+  })
+}
+
+app.post("/log", async (req, res) => {
+  // TODO: add validation
+  console.log(req.body)
+  await saveLog(req.body)
+  res.status(201).json()
 });
 
 app.listen(PORT, () => {
