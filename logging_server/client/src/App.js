02431ec0e2f0d4902c3f54889477863710680185
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2021-12-03 16:07:14 -0500

    added fetch to make request to /message endpt

--- logging_server/client/src/App.js
+++ logging_server/client/src/App.js
@@ -3,8 +3,26 @@ import "./App.css";
 import {createData, DataTable} from "./DataTable";
 import {StyledEngineProvider} from "@mui/material/styles";
 
+const handleChange = (event) => {
+  // Execute search here 
+  fetch("/logs/search/" + this.state.message)
+      .then(res => res.json())
+      .then(res => {
+        const rows = [];
+        res.map(x => {
+          rows.unshift(createData(x.id, x.message, x.logLevel, x.timestamp));
+        });
+        rows.sort((a, b) => {
+          return a.timestamp - b.timestamp;
+        });
+        setData(rows);
+      })
+      .catch(err => console.log(err));
+}
+
 function App() {
   const [data, setData] = React.useState([]);
+  const [message, setMessage] = React.useState([]);
   React.useEffect(() => {
     fetch("/logs/search")
       .then(res => res.json())
@@ -25,6 +43,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>{"Distributed Logging and Monitoring System"}</h2>
+        <input
+            type="text"
+            value={this.state.search}
+            onChange={this.handleChange}
+         />
         <StyledEngineProvider injectFirst>
           <DataTable rows={data}/>
         </StyledEngineProvider>
